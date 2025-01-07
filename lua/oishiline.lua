6773d1a5cf91d3c0ac718ee59a5daaf992fa3454
nix-on-droid <andrieee44@gmail.com>
2025-01-07 13:57:34 +0800

    tabline: Move tabline to its own file

--- lua/oishiline.lua
+++ lua/oishiline.lua
@@ -174,19 +174,7 @@ function M.statusline()
 end
 
 function M.tabline()
-	local currentTab = vim.fn.tabpagenr()
-	local results = {}
-	local j = 1
-
-	for i = 1, vim.fn.tabpagenr("$") do
-		local basename =
-			string.gsub(vim.api.nvim_buf_get_name(vim.fn.tabpagebuflist(i)[vim.fn.tabpagewinnr(i)]), ".*/", "")
-
-		results[j] = basename == "" and "[No Name]" or basename
-		j = j + 1
-	end
-
-	return table.concat(results)
+	return require("oishiline.tabline")
 end
 
 function M.setup(userArgs)
---
+++ lua/oishiline/tabline.lua
@@ -0,0 +1,12 @@
+local currentTab = vim.fn.tabpagenr()
+local results = {}
+local j = 1
+
+for i = 1, vim.fn.tabpagenr("$") do
+	local basename = string.gsub(vim.api.nvim_buf_get_name(vim.fn.tabpagebuflist(i)[vim.fn.tabpagewinnr(i)]), ".*/", "")
+
+	results[j] = basename == "" and "[No Name]" or basename
+	j = j + 1
+end
+
+return table.concat(results)
